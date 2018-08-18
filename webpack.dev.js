44377880cd34c10bec575560fc4bd6dc978abf5a
ZiedB <dziioza@gmail.com>
2018-08-18 12:42:23 +0100

    Intitial commit

---
+++ webpack.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: "development",
+    entry: "./src/js/app.js",
+    output: {
+        filename: "./app.bundle.js"
+    }
+}
---
+++ webpack.prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: "production",
+    entry: "./src/js/app.js",
+    output: {
+        filename: "./app.bundle.js"
+    }
+}
